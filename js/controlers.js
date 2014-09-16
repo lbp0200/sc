3ae877ad5fd2db8a0f673365f1793171597eb4c2
lbp0200 <lbp@lbp.im>
2014-09-16 18:01:42 +0800

    1

--- js/controlers.js
+++ js/controlers.js
@@ -1,9 +1,9 @@
 var phonecatControllers = angular.module('phonecatControllers', []);
 phonecatControllers.controller('PhoneListCtrl', function($scope, $http) {
-    $http.get('/phones.json').success(function(data) {
+    $http.get('phones.json').success(function(data) {
         $scope.phones = data;
     });
-    $http.get('/json/tags.json').success(function(data) {
+    $http.get('json/tags.json').success(function(data) {
         $scope.tags = data;
     });
     $scope.orderProp = "age";
