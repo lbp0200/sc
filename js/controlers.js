52e779dcac0d848ec45b156510676eda21e9ef10
lbp0200 <lbp@lbp.im>
2014-09-16 15:51:36 +0800

    1

--- js/controlers.js
+++ js/controlers.js
@@ -3,7 +3,9 @@ phonecatControllers.controller('PhoneListCtrl', function($scope, $http) {
     $http.get('/phones.json').success(function(data) {
         $scope.phones = data;
     });
-
+    $http.get('/json/tags.json').success(function(data) {
+        $scope.tags = data;
+    });
     $scope.orderProp = "age";
 });
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
